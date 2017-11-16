fc90934a59d645ef387668667537a03cb9bd791b
Mark Benus <mbenus@gmail.com>
2017-11-16 21:25:04 +0100

    robust connection

--- api.py
+++ api.py
@@ -1,18 +1,23 @@
 from flask import Flask, request
 from flask_restful import reqparse, abort, Api, Resource
+from flask_cors import CORS
 
 import pymysql.cursors
 import time
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
-connection = pymysql.connect(host='localhost',
+
+def openMysql():
+    return pymysql.connect(host='localhost',
                              user='MYSQL_USER',
                              password='MYSQL_PASSWORD',
                              db='moneropoel',
                              charset='utf8',
                              cursorclass=pymysql.cursors.DictCursor)
+connection = openMysql()
 
 # IP authorization #############################
 admins_ips = ['127.0.0.1']
@@ -34,55 +39,80 @@ parser.add_argument('task')
 class Miner(Resource):
     def get(self, minerId):
         isadmin()
-        
-        sql = "SELECT id, address, UNIX_TIMESTAMP(created) as created FROM miners WHERE id=%s" 
-        with connection.cursor() as cursor:
-            cursor.execute(sql, (minerId))
-            miner = cursor.fetchone()
-            return miner
+        def getData():
+            sql = "SELECT id, address, UNIX_TIMESTAMP(created) as created FROM miners WHERE id=%s" 
+            with connection.cursor() as cursor:
+                cursor.execute(sql, (minerId))
+                miner = cursor.fetchone()
+                return miner
+        try:
+            return getData()
+        except:
+            connection = openMysql()
+        return getData()
 
 class Miners(Resource):
     def get(self):
         isadmin()
+        
+        def getData():
+            sql = "SELECT id, address, UNIX_TIMESTAMP(created) as created FROM miners"
+            with connection.cursor() as cursor:
+                cursor.execute(sql)
+                miners = cursor.fetchall()
+                return miners
             
-        sql = "SELECT id, address, UNIX_TIMESTAMP(created) as created FROM miners"
-        with connection.cursor() as cursor:
-            cursor.execute(sql)
-            miners = cursor.fetchall()
-            return miners
+        try:
+            return getData()
+        except:
+            connection = openMysql()
+        return getData()
 
 
 class MinerStats(Resource):
     def get(self, minerId):
         isadmin()
+        
+        def getData():
+            to = time.time()
+            fr = to - (60 * 60 * 24 * 1) # 3 dagen
 
-        to = time.time()
-        fr = to - (60 * 60 * 24 * 3) # 3 dagen
-
-        select = "SELECT id, hashes, lastshare, balance, UNIX_TIMESTAMP(created) as created FROM minerstats"
-        where = " WHERE minerId=%s AND created >= FROM_UNIXTIME(%s)"
-        order = " ORDER BY created asc"
-        sql = select + where + order
-        with connection.cursor() as cursor:
-            cursor.execute(sql, (minerId, fr))
-            miners = cursor.fetchall()
-            return miners
+            select = "SELECT id, hashes, lastshare, balance, UNIX_TIMESTAMP(created) as created FROM minerstats"
+            where = " WHERE minerId=%s AND created >= FROM_UNIXTIME(%s)"
+            order = " ORDER BY created asc"
+            sql = select + where + order
+            with connection.cursor() as cursor:
+                cursor.execute(sql, (minerId, fr, to))
+                miners = cursor.fetchall()
+                return miners
+        try:
+            return getData()
+        except:
+            connection = openMysql()
+        return getData()
 
 class MoneroPrices(Resource):
     def get(self, target):
-        isadmin()
+        #isadmin()
 
-        to = time.time()
-        fr = to - (60 * 60 * 24 * 3) # 3 dagen
+        def getData():
+            to = time.time()
+            fr = to - (60 * 60 * 24 * 3) # 3 dagen
 
-        select = "SELECT price as y, UNIX_TIMESTAMP(created) as x FROM moneroprices"
-        where = " WHERE target=%s AND timestamp >= FROM_UNIXTIME(%s)"
-        order = " ORDER BY x asc"
-        sql = select + where + order
-        with connection.cursor() as cursor:
-            cursor.execute(sql, (target, fr))
-            prices = cursor.fetchall()
-            return prices
+            select = "SELECT price as y, UNIX_TIMESTAMP(created) as x FROM moneroprices"
+            where = " WHERE target=%s AND timestamp >= FROM_UNIXTIME(%s)"
+            order = " ORDER BY x asc"
+            sql = select + where + order
+            with connection.cursor() as cursor:
+                cursor.execute(sql, (target, fr))
+                prices = cursor.fetchall()
+                return prices
+
+        try:
+            return getData()
+        except:
+            connection = openMysql()
+        return getData()
 
 ##
 ## Actually setup the Api resource routing here
